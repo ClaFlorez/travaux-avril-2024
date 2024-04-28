0e79c715f5e2b6a4f50bfc3d38cc7e08f79ef5c1
ClaFlorez <168297735+ClaFlorez@users.noreply.github.com>
2024-04-28 09:38:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Additionner
+{
+    class Program
+    {
+        // Méthode principale de l’application.
+        static void Main(string[] args)
+        {
+            // Section de déclaration des variables
+            int Nombre_1, Nombre_2, Nombre_3, Total;
+            // Saisie du premier nombre.
+            Console.Write("Donnez - moi le premier nombre entier: ");
+            Nombre_1 = Convert.ToInt16(Console.ReadLine());
+            // Saisie du deuxième nombre.
+            Console.Write("Donnez - moi le deuxième nombre entier:");
+            Nombre_2 = Convert.ToInt16(Console.ReadLine());
+            // Saisie du troisième nombre.
+            Console.Write("Donnez - moi le troisième nombre entier:");
+            Nombre_3 = Convert.ToInt16(Console.ReadLine());
+            // Opération d’addition.
+            Total = Nombre_1 + Nombre_2 + Nombre_3;
+            // Affichage du résultat et fin du programme.
+            Console.WriteLine(Nombre_1 + "+" +Nombre_2 + "+" +Nombre_3 + "=" +Total);
+            Console.ReadKey();
+        }
+    }
+}
